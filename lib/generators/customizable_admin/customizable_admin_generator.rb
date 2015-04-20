badfd6de59120b8b8231e8fa408f3a0fade8938d
Julien Carbonnier <julien.carbonnier@digitm.eu>
2015-04-20 23:20:36 +0200

    Update project

--- lib/generators/customizable_admin/customizable_admin_generator.rb
+++ lib/generators/customizable_admin/customizable_admin_generator.rb
@@ -3,11 +3,23 @@ require 'rails/generators/rails/scaffold/scaffold_generator'
 class CustomizableAdminGenerator < Rails::Generators::ScaffoldGenerator
 end
 
+require 'rails/generators/rails/scaffold_controller/scaffold_controller_generator'
+require 'rails/generators/resource_helpers'
+
+module Rails
+  module Generators
+    class ScaffoldControllerGenerator < NamedBase
+      source_root File.expand_path('../../../customizable_admin/templates/rails/scaffold_controller', __FILE__)
+    end
+  end
+end
+
 require 'rails/generators/erb/scaffold/scaffold_generator'
 
 module Erb
   module Generators
     class ScaffoldGenerator < Base
+      source_root File.expand_path('../../../customizable_admin/templates/erb/scaffold', __FILE__)
 
       protected
 
