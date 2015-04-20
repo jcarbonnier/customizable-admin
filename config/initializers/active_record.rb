f5ee49736b31d03f266170c910ae7a6eb1213b6a
Julien Carbonnier <julien.carbonnier@digitm.eu>
2015-04-20 21:40:41 +0200

    Debug search

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -6,21 +6,19 @@ module ActiveRecord
     #-----
     def self.search(in_parameters = {})
       return all if (in_parameters.blank?)
-      conditions = []
+      conditions = ["1=1"]
       parameters = in_parameters.delete_if { |key, v| [].include?(key) or v.blank? or v.empty? or (v.class == Array and (v - ['']).empty?) }
       parameters.each do |k, v|
         Rails.logger.info("K:#{k}=#{v}|blank:#{v.blank?}|empty:#{v.empty?}|#{v.inspect}")
         case k
           when 'action', 'controller', '_', 'per_page', 'page', 'utf8', 'sort', 'direction', 'format', 'columns'
           when 'id'
-            conditions[0] ||= ''
             conditions[0] << " AND #{self.table_name}.#{k}=?"
             conditions.push(v)
           # when 'role_ids'
           #   conditions[0] << " AND roles_users.role_id in (?)"
           #   conditions.push(v)
           else
-            conditions[0] ||= ''
             conditions[0] << " AND #{self.table_name}.#{k} like ?"
             conditions.push("%#{v}%")
         end
