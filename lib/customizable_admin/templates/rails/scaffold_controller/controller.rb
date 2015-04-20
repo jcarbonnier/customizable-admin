c45dda3d3fd7b489539e509e383055f62c0051c9
Julien Carbonnier <julien.carbonnier@digitm.eu>
2015-04-21 01:16:27 +0200

    Update generated controller

--- lib/customizable_admin/templates/rails/scaffold_controller/controller.rb
+++ lib/customizable_admin/templates/rails/scaffold_controller/controller.rb
@@ -3,7 +3,7 @@ require_dependency "<%= namespaced_file_path %>/application_controller"
 
 <% end -%>
 <% module_namespacing do -%>
-class <%= controller_class_name %>Controller < ApplicationController
+class <%= controller_class_name %>Controller < CustomizableAdmin::AuthorizationsController
 
   #----------------------------------------
   protected
